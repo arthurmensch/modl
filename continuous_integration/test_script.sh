c1c363584f7124217ceef64103cf2166c83c8d3c
Arthur Mensch <arthur.mensch@m4x.org>
2016-05-24 15:53:30 +0200

    Travis

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -3,11 +3,15 @@
 set -e
 
 if [[ "$SKIP_TESTS" != "true" ]]; then
-    python continuous_integration/show-python-packages-versions.py
+    python continuous_integration/show-python-packages-versions.py;
     # We want to back out of the current working directory to make
     # sure we are using nilearn installed in site-packages rather
     # than the one from the current working directory
     # Parentheses (run in a subshell) are used to leave
     # the current directory unchanged
-    (cd "$TEST_RUN_FOLDER" && make -f $OLDPWD/Makefile test-coverage)
+    if [[ "$COVERAGE" == "true" ]]; then
+        (cd "$TEST_RUN_FOLDER" && make -f $OLDPWD/Makefile test-coverage)
+    else
+        (cd "$TEST_RUN_FOLDER" && make -f $OLDPWD/Makefile test)
+    fi
 fi
