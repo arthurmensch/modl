d7e235adf776317974bd1cffa73d6e3c378f753d
MENSCH Arthur <arthur.mensch@m4x.org>
2017-02-16 13:14:32 +0100

    Before remasking

--- examples/contrast/predict_contrast.py
+++ examples/contrast/predict_contrast.py
@@ -1,6 +1,6 @@
-import shutil
+import os
+
 from os.path import join
-from tempfile import mkdtemp
 
 import pandas as pd
 import numpy as np
@@ -15,7 +15,7 @@ from sklearn.preprocessing import LabelEncoder
 from modl.classification import MultiProjectionTransformer, \
     L2LogisticRegressionCV
 from modl.datasets import get_data_dirs
-from modl.input_data.fmri.raw_masker import get_raw_contrast_data
+from modl.input_data.fmri.unmask import get_raw_contrast_data
 from modl.utils.system import get_cache_dirs
 
 prediction_ex = Experiment('prediction')
@@ -30,7 +30,8 @@ def config():
     seed = 2
     max_iter = 100
     tol = 1e-3
-    dump_dir = join(get_data_dirs()[0], 'raw', 'hcp', 'task')
+    alpha = 1e-4
+
     n_components_list = [16, 64, 256]
     test_size = 0.1
     train_size = None
@@ -38,20 +39,26 @@ def config():
 
 
 @prediction_ex.automain
-def run(standardize, C, tol, n_components_list, max_iter, n_jobs,
+def run(standardize, C, tol,
+        n_components_list,
+        alpha,
+        max_iter, n_jobs,
         test_size,
         train_size,
-        dump_dir,
         verbose,
         n_subjects,
         _run,
         _seed):
     memory = Memory(cachedir=get_cache_dirs()[0])
 
-    # Fetched unmasked data and label them$
-    _run.info['stage'] = 'fetching data'
-    X = get_raw_contrast_data(dump_dir)
+    unmask_contrast_dir = join(get_data_dirs()[0], 'pipeline',
+                               'unmask', 'contrast', 'hcp')
+
+    # Fetched unmasked data and label them
+    X = get_raw_contrast_data(unmask_contrast_dir)
+
     subjects = X.index.get_level_values('subject').unique().values.tolist()
+
     subjects = subjects[:n_subjects]
     X = X.loc[subjects]
 
@@ -63,8 +70,8 @@ def run(standardize, C, tol, n_components_list, max_iter, n_jobs,
     train_subjects, test_subjects = \
         train_test_split(subjects, random_state=_seed, test_size=test_size)
     train_subjects = train_subjects[:train_size]
-    _run.info['pred_train_subject'] = train_subjects
-    _run.info['pred_test_subjects'] = test_subjects
+    _run.info['train_subject'] = train_subjects
+    _run.info['test_subjects'] = test_subjects
 
     X = pd.concat([X.loc[train_subjects],
                    X.loc[test_subjects]], keys=['train', 'test'],
@@ -73,10 +80,8 @@ def run(standardize, C, tol, n_components_list, max_iter, n_jobs,
                    y.loc[test_subjects]], keys=['train', 'test'],
                   names=['fold'])
     # Components
-    _run.info['stage'] = 'fetching components'
-    components_dir = '_'.join(map(str, n_components_list))
-    components_dir = join(get_data_dirs()[0], 'hierachical', components_dir)
-    components_imgs = [join(components_dir,
+    components_dir = join(get_data_dirs()[0], 'pipeline', 'components', 'hcp')
+    components_imgs = [join(components_dir, this_n_components, alpha,
                             'components_%i.nii.gz' % this_n_components)
                        for this_n_components in n_components_list]
     mask_img = fetch_hcp_mask(data_dir=join(get_data_dirs()[0], 'HCP900'))
@@ -104,11 +109,9 @@ def run(standardize, C, tol, n_components_list, max_iter, n_jobs,
                                                   memory_level=1,
                                                   n_jobs=n_jobs)
     piped_classifier = make_pipeline(imgs_transformer, l1_log_classifier)
-    _run.info['stage'] = 'model fitting'
     piped_classifier.fit(X.loc['train'].values,
                          y.loc['train'].values)
 
-    _run.info['stage'] = 'model eval'
     predicted_y = piped_classifier.predict(X.values)
     predicted_labels = label_encoder.inverse_transform(predicted_y)
     true_labels = label_encoder.inverse_transform(y.values)
@@ -128,16 +131,14 @@ def run(standardize, C, tol, n_components_list, max_iter, n_jobs,
 
     _run.info['test_score'] = test_score
 
-    _run.info['stage'] = 'dumping results'
-    if not _run.unobserved:
-        artifact_dir = mkdtemp()
-        _run.info['pred_train_score'] = train_score
-        _run.info['pred_test_score'] = test_score
-        print('Write task prediction artifacts')
-        prediction.to_csv(join(artifact_dir, 'prediction.csv'))
-        _run.add_artifact(join(artifact_dir, 'prediction.csv'),
-                          name='prediction.csv')
-        try:
-            shutil.rmtree(artifact_dir)
-        except FileNotFoundError:
-            pass
+    _run.info['train_score'] = train_score
+    _run.info['test_score'] = test_score
+    print('Write task prediction artifacts')
+
+    artifact_dir = join(get_data_dirs()[0], 'contrast', 'prediction')
+    if not os.path.exists:
+        os.makedirs(artifact_dir)
+
+    prediction.to_csv(join(artifact_dir, 'prediction.csv'))
+    _run.add_artifact(join(artifact_dir, 'prediction.csv'),
+                      name='prediction.csv')
