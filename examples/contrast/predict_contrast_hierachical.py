034f113e261462fd8e2eec9200f5d71fe2a60784
MENSCH Arthur <arthur.mensch@m4x.org>
2017-04-14 10:21:59 +0200

    WIP

---
+++ examples/contrast/predict_contrast_hierachical.py
@@ -0,0 +1,161 @@
+import os
+from os.path import join
+
+import numpy as np
+import pandas as pd
+from sacred import Experiment
+from sacred.observers import MongoObserver
+from sklearn.externals.joblib import load
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer
+
+from modl.datasets import get_data_dirs
+from modl.hierarchical import make_model, init_tensorflow
+from modl.model_selection import StratifiedGroupShuffleSplit
+
+idx = pd.IndexSlice
+
+predict_contrast = Experiment('predict_contrast_hierachical')
+collection = predict_contrast.path
+
+global_artifact_dir = join(get_data_dirs()[0], 'pipeline', 'contrast',
+                           'prediction_hierachical')
+
+# observer = MongoObserver.create(db_name='amensch', collection=collection)
+# predict_contrast.observers.append(observer)
+
+
+# @predict_contrast.config
+# def config():
+reduced_dir = join(get_data_dirs()[0], 'pipeline', 'contrast', 'reduced')
+
+datasets = ['hcp', 'archi']
+test_size = dict(hcp=0.1, archi=0.5, la5c=0.5, brainomics=0.5)
+n_subjects = dict(hcp=None, archi=None, la5c=None, brainomics=None)
+dataset_weight = dict(hcp=1, archi=1, la5c=1, brainomics=1)
+train_size = None
+
+validation = True
+
+alpha = 0.0001
+latent_dim = 200
+activation = 'linear'
+dropout_input = 0.25
+dropout_latent = 0.9
+batch_size = 100
+optimizer = 'adam'
+epochs = 100
+
+depth_probs = [0., 0.5, 0.5]
+
+# projection = True
+
+n_jobs = 24
+verbose = 2
+seed = 10
+
+
+# @predict_contrast.automain
+# def run(alpha,
+#         latent_dim,
+#         n_subjects,
+#         test_size,
+#         train_size,
+#         dropout_input,
+#         dropout_latent,
+#         activation,
+#         datasets,
+#         depth_probs,
+#         reduced_dir,
+#         optimizer,
+#         batch_size,
+#         epochs,
+#         verbose,
+#         n_jobs,
+#         _run,
+#         _seed):
+artifact_dir = join(global_artifact_dir,
+                    '10', '_artifacts')
+if not os.path.exists(artifact_dir):
+    os.makedirs(artifact_dir)
+
+if verbose:
+    print('Fetch data')
+
+X = []
+for dataset in datasets:
+    this_reduced_dir = join(reduced_dir, dataset)
+    this_X = load(join(this_reduced_dir, 'Xt.pkl'), mmap_mode='r')
+    subjects = this_X.index.get_level_values('subject'). \
+        unique().values.tolist()
+    subjects = subjects[:n_subjects[dataset]]
+    this_X = this_X.loc[idx[subjects]]
+    X.append(this_X)
+X = pd.concat(X, keys=datasets, names=['dataset'])
+X.sort_index(inplace=True)
+
+# Building numpy array
+y = np.zeros((X.shape[0], 3), dtype=np.int32)
+le_dict = {}
+for i, label in enumerate(['dataset', 'task', 'contrast']):
+    le = LabelEncoder()
+    y[:, i] = le.fit_transform(X.index.get_level_values(label).values)
+    le_dict[label] = le
+
+label_pool = np.vstack({tuple(row) for row in y})
+indices = np.argsort(label_pool[:, -1])
+label_pool = label_pool[indices]
+# d is only used at test time. 1: per dataset. 2: per task
+d = np.ones(y.shape[0])
+
+lbin = LabelBinarizer()
+contrasts = y[:, -1]
+contrasts_oh = lbin.fit_transform(contrasts)
+n_features = X.shape[1]
+x = X.values
+
+dataset_length = X.iloc[:, 0].groupby(level='dataset').transform('count').values
+sample_weight = 1. / dataset_length
+sample_weight /= np.min(sample_weight)
+
+# Cross validation folds
+cv = StratifiedGroupShuffleSplit(stratify_levels='dataset',
+                                 group_name='subject',
+                                 test_size=test_size,
+                                 train_size=train_size,
+                                 n_splits=1,
+                                 random_state=0)
+train, test = next(cv.split(X))
+
+init_tensorflow(n_jobs=n_jobs, debug=True)
+
+model = make_model(n_features,
+                   alpha=alpha,
+                   latent_dim=latent_dim,
+                   activation=activation,
+                   dropout_input=dropout_input,
+                   dropout_latent=dropout_latent,
+                   optimizer=optimizer,
+                   label_pool=label_pool,
+                   seed=0,
+                   depth_probs=depth_probs,
+                   shared_supervised=False)
+model.fit(x=[x[train], y[train], d[train]],
+          y=contrasts_oh[train], sample_weight=sample_weight[train],
+          batch_size=batch_size,
+          epochs=epochs)
+# use only y[:, :d] as input
+y_pred_oh = model.predict(x=[x, y, d])
+
+y_pred = lbin.inverse_transform(y_pred_oh)
+true_contrast = le_dict['contrast'].inverse_transform(contrasts)
+predicted_contrast = le_dict['contrast'].inverse_transform(y_pred)
+prediction = pd.DataFrame({'true_label': true_contrast,
+                           'predicted_label': predicted_contrast},
+                          index=X.index)
+prediction = pd.concat([prediction.iloc[train],
+                        prediction.iloc[test]],
+                       names=['fold'], keys=['train', 'test'])
+prediction.to_csv('prediction.csv')
+prediction.to_csv(join(artifact_dir, 'prediction.csv'))
+model.save(join(model, 'model.keras'))
+
--- modl/hierarchical.py
+++ modl/hierarchical.py
@@ -1,9 +1,13 @@
-import functools
-
-import numpy as np
 import keras.backend as K
+import numpy as np
+import tensorflow as tf
+from keras.backend import set_session
 from keras.engine import Layer, Model, Input
 from keras.layers import Dense, Dropout
+from keras.regularizers import l2
+from keras.utils import to_categorical
+from tensorflow.contrib.distributions import Categorical
+from tensorflow.python import debug as tf_debug
 
 
 class PartialSoftmax(Layer):
@@ -15,11 +19,11 @@ class PartialSoftmax(Layer):
 
     def call(self, inputs, indices):
         logits = inputs
-        logits_max = K.reduce_max(logits, axis=1)
+        logits_max = tf.reduce_max(logits, axis=1)
         logits -= logits_max[:, np.newaxis]
-        exp_logits = K.where(indices, K.exp(logits),
-                             K.zeros(K.shape(logits)))
-        sum_exp_logits = K.reduce_sum(exp_logits)
+        exp_logits = tf.where(indices, tf.exp(logits),
+                              tf.zeros(tf.shape(logits)))
+        sum_exp_logits = tf.reduce_sum(exp_logits)
         return exp_logits / sum_exp_logits
 
     def compute_output_shape(self, input_shape):
@@ -29,48 +33,58 @@ class PartialSoftmax(Layer):
 def partial_sparse_categorical_crossentropy_with_logits(y_true,
                                                         y_pred, indices):
     logits = y_pred
-    logits_max = K.reduce_max(logits, axis=1)
+    logits_max = tf.reduce_max(logits, axis=1)
     logits = y_pred - logits_max[:, np.newaxis]
-    exp_logits = K.where(indices, K.exp(logits), K.zeros(K.shape(logits)))
+    exp_logits = tf.where(indices, tf.exp(logits), tf.zeros(tf.shape(logits)))
 
-    log_prob = logits - K.log(K.reduce_sum(exp_logits))
+    log_prob = logits - tf.log(tf.reduce_sum(exp_logits))
 
-    cross_entropy = K.reduce_sum(K.gather(log_prob, y_true))
+    cross_entropy = tf.reduce_sum(tf.gather(log_prob, indices=y_true))
 
     return cross_entropy
 
 
 class HierachicalLabelMasking(Layer):
-    def __init__(self, labels,
-                 seed=None,
+    def __init__(self, label_pool,
+                 depth_probs,
+                 random_at_training,
+                 seed,
                  **kwargs):
+        self.labels = label_pool
+        self.depth_probs = depth_probs
+        self.random_at_training = random_at_training
         self.seed = seed
-        self.labels = labels
         super(HierachicalLabelMasking, self).__init__(**kwargs)
 
     def build(self, input_shape):
         n_labels, max_depth = self.labels.shape
-        adversaries = np.zeros((max_depth, n_labels, n_labels))
-        for depth in range(max_depth):
-            adversaries[depth] = labels[:, [depth]] == labels[:, depth]
-        adversaries = K.constant(adversaries, dtype=np.bool)
-        self.add_weight(K.shape(adversaries),
-                        initializer=lambda: adversaries,
-                        name='adversaries', trainable=False)
+        adversaries = np.ones((max_depth, n_labels, n_labels))
+        for depth in range(1, max_depth):
+            adversaries[depth] = self.labels[:, [depth - 1]] == self.labels[:,
+                                                                depth - 1]
+        self.adversaries = tf.constant(adversaries, dtype=np.bool)
+        self.max_depth = adversaries.shape[0]
         super(HierachicalLabelMasking, self).build(input_shape)
 
-    def call(self, y):
-        adversaries = self.get_weights()[0]
-        max_depth = K.shape(adversaries)[0]
-        y_leaf = y[:, -1]
-        n_samples = K.shape(y_leaf)[0]
-        depths = K.random_uniform(shape=[n_samples],
-                                  maxval=max_depth, dtype=np.int32)
-        y_at_depth = K.gather_nd(y, indices=K.concat(
-            [K.range(n_samples)[:, np.newaxis], depths[:, np.newaxis]],
-            axis=1))
-        indices = K.gather_nd(adversaries, indices=K.concat(
-            [depths[:, np.newaxis], y_at_depth[:, np.newaxis]], axis=1))
+    def call(self, y, depths, training=None):
+        # Depths is simply ignored during training time
+        y_leaf = y[:, -1][:, np.newaxis]
+        n_samples = tf.shape(y_leaf)[0]
+
+        def random_depths():
+            dist = Categorical(probs=self.depth_probs, dtype=np.int32,
+                               name='random_depths')
+            return dist.sample(sample_shape=[n_samples, 1], seed=self.seed)
+
+        depths = K.in_train_phase(random_depths
+                                  if self.random_at_training
+                                  else depths, depths, training=training)
+
+        # y_at_depth = tf.gather_nd(y, indices=tf.concat(
+        #     [tf.range(n_samples)[:, np.newaxis], depths], axis=1))
+
+        indices = tf.gather_nd(self.adversaries, indices=tf.concat(
+            [depths, y_leaf], axis=1))
         return indices
 
     def compute_output_shape(self, input_shape):
@@ -89,35 +103,97 @@ def create_dataset():
                        [1, 3, 8],
                        [1, 3, 9],
                        [1, 3, 10]], dtype=np.int32)
-    n_samples = 10000
+    n_samples = 1000
     y_indices = np.random.randint(11, size=n_samples)
     y = labels[y_indices]
-    x = np.random.randn(n_samples, 100)
+    x = np.random.randn(n_samples, 1000)
     x = x.astype(np.float32)
     return x, y, labels
 
-X, y, labels = create_dataset()
 
-n_features = X.shape[1]
-n_classes = labels.shape[0]
+def make_model(n_features, alpha,
+               latent_dim, dropout_input,
+               dropout_latent,
+               optimizer,
+               activation,
+               seed,
+               depth_probs, label_pool,
+               shared_supervised):
+    n_classes = label_pool.shape[0]
+    data = Input(shape=(n_features,), name='data')
+    labels = Input((3,), name='labels', dtype=np.int32)
+    # Unused at test time
+    depths = Input((1,), name='depths', dtype=np.int32)
+    # Dropout
+    dropout_data = Dropout(rate=dropout_input, seed=seed)(data)
+    latent = Dense(latent_dim, activation=activation,
+                   use_bias=False, name='latent',
+                   kernel_regularizer=l2(alpha))(dropout_data)
+    latent_dropout = Dropout(rate=dropout_latent, name='dropout',
+                             seed=seed)(latent)
+    if shared_supervised:
+        indices = HierachicalLabelMasking(
+            label_pool=label_pool,
+            depth_probs=depth_probs,
+            random_at_training=True,
+            seed=seed,
+            name='label_masking')(
+            labels, depths=depths)
+        logits = Dense(n_classes, activation='linear',
+                       use_bias=True,
+                       kernel_regularizer=l2(alpha),
+                       name='supervised')(latent_dropout)
+        prob = PartialSoftmax()(logits, indices=indices)
+    else:
+        dist = Categorical(probs=depth_probs, dtype=np.int32,
+                           name='random_depths')
+        depth = dist.sample(sample_shape=[1], seed=seed)
+        logits = []
+        prob_list = []
+        for i in range(3):
+            logits[i] = Dense(n_classes, activation='linear',
+                              use_bias=True,
+                              kernel_regularizer=l2(alpha),
+                              name='supervised')(latent_dropout)
+            prob_list[i] = PartialSoftmax()(logits, indices=indices)
+        prob = tf.case({tf.equal(depth, i): lambda: prob_list[i]
+                        for i in range(3)})
+    model = Model(inputs=[data, labels, depths], outputs=prob)
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
 
-latent_dim = 10
-dropout_rate = 0.5
 
-input = Input(shape=(n_features, ), name='input')
-input_labels = Input((3, ), name='labels')
-# Dropout
-latent = Dense(latent_dim, activation='linear',
-               use_bias=False, name='latent')(input)
-latent_dropout = Dropout(rate=0.5, name='dropout')(latent)
-logits = Dense(n_classes, activation='linear',
-               use_bias=True, name='supervised')(latent_dropout)
+def init_tensorflow(n_jobs=1, debug=False):
+    sess = tf.Session(
+        config=tf.ConfigProto(
+            device_count={'CPU': n_jobs},
+            inter_op_parallelism_threads=n_jobs,
+            intra_op_parallelism_threads=n_jobs,
+            use_per_session_threads=True)
+    )
+    if debug:
+        sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+    set_session(sess)
 
-indices = HierachicalLabelMasking(labels=labels, seed=None)(input_labels)
-prob = PartialSoftmax()(logits, indices=indices)
-loss = functools.partial(partial_sparse_categorical_crossentropy_with_logits,
-                         indice=indices)
-training_model = Model(input=[input, input_labels], output=logits)
-training_model.compile('adam', loss=loss)
 
-test_model = Model(input=input, output=prob)
+def run():
+    dropout_rate = 0.5
+    latent_dim = 50
+
+    x, y, label_pool = create_dataset()
+    d = np.ones(y.shape[0])
+    y_oh = to_categorical(y[:, -1])
+
+    n_features = x.shape[1]
+
+    model = make_model(n_features, latent_dim, dropout_rate,
+                       label_pool)
+    model.fit(x=[x, y, d], y=y_oh, epochs=50)
+    loss = model.evaluate(x=[x, y, d], y=y_oh)
+    print()
+    print(loss)
+
+
+if __name__ == '__main__':
+    run()
