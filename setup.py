86d01ac49628f043a690a29a492d2e9ade5ffca4
MENSCH Arthur <arthur.mensch@inria.fr>
2017-06-01 13:58:21 +0200

    v0.6

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ MAINTAINER_EMAIL = 'arthur.mensch@m4x.org'
 URL = 'https://github.com/arthurmensch/modl'
 LICENSE = 'new BSD'
 DOWNLOAD_URL = 'https://github.com/arthurmensch/modl'
-VERSION = '0.5'
+VERSION = '0.6'
 
 
 def configuration(parent_package='', top_path=None):
